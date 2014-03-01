7c1f5ce58ab66356f48f303518ad3c71ceb54ecf
Chris Zalidis <zalidis@gmail.com>
2014-03-01 18:21:21 +0200

    refactored launchers, only for hole

--- pandora_vision_hole/include/pandora_vision_hole/hole_detection.h
+++ pandora_vision_hole/include/pandora_vision_hole/hole_detection.h
@@ -35,7 +35,7 @@
 * Author: Michael Skolarikis
 *********************************************************************/
 
- 
+
 #ifndef HOLEFINDNODE_H
 #define  HOLEFINDNODE_H
 #define DEBUG_HOLES 0
@@ -67,96 +67,97 @@
 #define DEFAULT_HEIGHT			480		//default frame height
 #define DEFAULT_WIDTH			640		//default frame width
 
-class HoleFindNode : public StateClient 
+class HoleFindNode : public StateClient
 {
-	private:
+private:
 
-		//nodeHandle
-		ros::NodeHandle 	_nh;
-		HoleFinder*			_holeFinder;
-		Tracker*			_tracker;
-		
-		float ratioX;
-		float ratioY;	
-		float hfov;			//horizontal Field Of View (rad)
-		float vfov;			//vertical Field Of View (rad)
-		int frameNum;		//current frame id
-		int frameWidth;		//frame width
-		int frameHeight;	//frame height
-		
-		std::string packagePath;
-		
-		IplImage*		holeFrame;					// frame processed by HoleFinder
-		IplImage*		extraFrame;					// copy frame processed by HoleFinder
-		
-		ros::Time		holeFrameTimestamp;		// holeFinder frame timestamp
+  //nodeHandle
+  ros::NodeHandle 	_nh;
+  HoleFinder*			_holeFinder;
+  Tracker*			_tracker;
 
-		string imageTopic;
-		
-		//time durations for every callback Timer in spin() function
-		double holeTime;
-		
-		
-		//publishers for HoleFinder result messages
-		ros::Publisher _holesDirectionPublisher;
-		
-		//the subscriber that listens to the frame topic advertised by the central node
-		image_transport::Subscriber _frameSubscriber;
-		
-		//debug publishers for HoleFinder
-		image_transport::Publisher _holeSourcePublisher;
-		image_transport::Publisher _holeEdgePublisher;
-		image_transport::Publisher _holeThresholdPublisher;
-		image_transport::Publisher _holeResultPublisher;
-		image_transport::Publisher _holeTexturePublisher;
-		
-		//debug publishers for Tracker
-		image_transport::Publisher _trackerChainPublisher1;
-		image_transport::Publisher _trackerChainPublisher2;
-		image_transport::Publisher _trackerChainPublisher3;
-		image_transport::Publisher _trackerChainPublisher4;
-		image_transport::Publisher _trackerChainPublisher5;
-		
-		image_transport::Publisher _camShiftPublisher;
-		
-		// variables for changing in dummy msg mode for debugging
-		bool holeDummy;
-		// variables for changing in debug mode. Publish images for debugging
-		bool debugHole;
-		
-		//variable used for State Managing
-		bool holeNowON;
+  float ratioX;
+  float ratioY;
+  float hfov;			//horizontal Field Of View (rad)
+  float vfov;			//vertical Field Of View (rad)
+  int frameNum;		//current frame id
+  int frameWidth;		//frame width
+  int frameHeight;	//frame height
 
-		void debug_publisher(ros::Time, IplImage*, image_transport::Publisher, std::string);
-		void debug_holes(cv::Mat, std::vector<Thing*>);
-	public:
-				
-		//constructor
-		HoleFindNode();
-					
-		//destructor			
-		~HoleFindNode();	
-		
-		//get parameters from launch file
-		void getGeneralParams();
-		void getHoleParams();
-		void getTrackerParams();
-			
-		//timer callbacks
-		void holeCallback(const ros::TimerEvent&);
-		void holeCallback();
-			
-		//get a new image
-		void imageCallback(const sensor_msgs::ImageConstPtr& msg);
-		
-		//Implemented from StateClient
-		void startTransition(int newState);
-		void completeTransition(void);
-		
-		int curState;		//Current state of robot
-		int prevState;		//Previous state of robot
+  std::string packagePath;
+  std::string imageTopic;
+  std::string cameraName; 
+  std::string cameraFrameId; 
+
+  IplImage*		holeFrame;					// frame processed by HoleFinder
+  IplImage*		extraFrame;					// copy frame processed by HoleFinder
+
+  ros::Time		holeFrameTimestamp;		// holeFinder frame timestamp
+
+  //time durations for every callback Timer in spin() function
+  double holeTime;
+
+
+  //publishers for HoleFinder result messages
+  ros::Publisher _holesDirectionPublisher;
+
+  //the subscriber that listens to the frame topic advertised by the central node
+  image_transport::Subscriber _frameSubscriber;
+
+  //debug publishers for HoleFinder
+  image_transport::Publisher _holeSourcePublisher;
+  image_transport::Publisher _holeEdgePublisher;
+  image_transport::Publisher _holeThresholdPublisher;
+  image_transport::Publisher _holeResultPublisher;
+  image_transport::Publisher _holeTexturePublisher;
+
+  //debug publishers for Tracker
+  image_transport::Publisher _trackerChainPublisher1;
+  image_transport::Publisher _trackerChainPublisher2;
+  image_transport::Publisher _trackerChainPublisher3;
+  image_transport::Publisher _trackerChainPublisher4;
+  image_transport::Publisher _trackerChainPublisher5;
+
+  image_transport::Publisher _camShiftPublisher;
+
+  // variables for changing in dummy msg mode for debugging
+  bool holeDummy;
+  // variables for changing in debug mode. Publish images for debugging
+  bool debugHole;
+
+  //variable used for State Managing
+  bool holeNowON;
+
+  void debug_publisher(ros::Time, IplImage*, image_transport::Publisher, std::string);
+  void debug_holes(cv::Mat, std::vector<Thing*>);
+public:
+
+  //constructor
+  HoleFindNode();
+
+  //destructor
+  ~HoleFindNode();
+
+  //get parameters from launch file
+  void getGeneralParams();
+  void getHoleParams();
+  void getTrackerParams();
+
+  //timer callbacks
+  void holeCallback(const ros::TimerEvent&);
+  void holeCallback();
+
+  //get a new image
+  void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+
+  //Implemented from StateClient
+  void startTransition(int newState);
+  void completeTransition(void);
+
+  int curState;		//Current state of robot
+  int prevState;		//Previous state of robot
 };
 
 #endif
-		
-		
+
+
