9cfcd1e99e303cc954a683b2dddba4d79748fd05
paschalidoud <paschalidoud@gmail.com>
2014-03-14 17:35:54 +0200

    changed state_client datamatrix_detection.h

--- pandora_vision_datamatrix/include/pandora_vision_datamatrix/datamatrix_detection.h
+++ pandora_vision_datamatrix/include/pandora_vision_datamatrix/datamatrix_detection.h
@@ -45,7 +45,7 @@
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
-#include "state_client.h"
+#include "state_manager/state_client.h"
 
 #include "datamatrix_detector.h"
 
