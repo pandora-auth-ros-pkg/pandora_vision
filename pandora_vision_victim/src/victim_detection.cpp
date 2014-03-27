d8fd6fcaed280b5f67fa64278cfb6d401bc2e3e4
paschalidoud <paschalidoud@gmail.com>
2014-03-27 14:06:32 +0200

    added class for color detection

--- pandora_vision_victim/src/victim_detection.cpp
+++ pandora_vision_victim/src/victim_detection.cpp
@@ -32,8 +32,7 @@
 *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 *  POSSIBILITY OF SUCH DAMAGE.
 *
-* Author: George Aprilis
-* 		  Despoina Paschalidou
+* Author: Despoina Paschalidou
 *********************************************************************/
 
 #include "pandora_vision_victim/victim_detection.h"
