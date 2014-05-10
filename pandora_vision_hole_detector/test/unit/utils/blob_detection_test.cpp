e3f49f4b8f3d7e4f4a05837ffc816cbb11f65e82
alexandros philotheou <alexandros.filotheou@gmail.com>
2014-05-10 18:59:24 +0300

    Adds BoundingBoxDetection tests
    Also, cleanup.
    
    	modified:   pandora_vision_hole_detector/include/utils/bounding_box_detection.h
    	modified:   pandora_vision_hole_detector/src/utils/bounding_box_detection.cpp
    	modified:   pandora_vision_hole_detector/src/utils/hole_filters.cpp
    	modified:   pandora_vision_hole_detector/test/CMakeLists.txt
    	modified:   pandora_vision_hole_detector/test/unit/utils/blob_detection_test.cpp
    
    	new file:   pandora_vision_hole_detector/test/unit/utils/bounding_box_detection_test.cpp

--- pandora_vision_hole_detector/test/unit/utils/blob_detection_test.cpp
+++ pandora_vision_hole_detector/test/unit/utils/blob_detection_test.cpp
@@ -1,39 +1,39 @@
 /*********************************************************************
-*
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2014, P.A.N.D.O.R.A. Team.
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the P.A.N.D.O.R.A. Team nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*
-* Author: Alexandros Philotheou
-*********************************************************************/
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2014, P.A.N.D.O.R.A. Team.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the P.A.N.D.O.R.A. Team nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Alexandros Philotheou
+ *********************************************************************/
 
 #include "utils/blob_detection.h"
 #include "gtest/gtest.h"
@@ -80,6 +80,7 @@ namespace pandora_vision
           square_.at< unsigned char >( 100, cols ) = 255;
           square_.at< unsigned char >( 199, cols ) = 255;
         }
+
         for ( int rows = 100; rows < 200; rows++)
         {
           square_.at< unsigned char >( rows, 100 ) = 255;
@@ -109,14 +110,13 @@ namespace pandora_vision
         // Construct the lower right square
         cv::Mat lowerRightSquare = cv::Mat::zeros(HEIGHT, WIDTH, CV_8UC1);
 
-        for ( int cols = WIDTH - 100; cols < WIDTH; cols++)
+        for ( int cols = WIDTH - 100; cols < WIDTH; cols++ )
         {
           lowerRightSquare.at< unsigned char >( HEIGHT - 100, cols ) = 255;
           lowerRightSquare.at< unsigned char >( HEIGHT - 1, cols ) = 255;
         }
 
-
-        for ( int rows = HEIGHT - 100; rows < HEIGHT ; rows++)
+        for ( int rows = HEIGHT - 100; rows < HEIGHT ; rows++ )
         {
           lowerRightSquare.at< unsigned char >( rows, WIDTH - 100 ) = 255;
           lowerRightSquare.at< unsigned char >( rows, WIDTH - 1 ) = 255;
@@ -173,7 +173,7 @@ namespace pandora_vision
       // of the square in the square_ image
       std::vector< cv::Point2f > squareOutlinePointsVector_;
 
-      // The vector holding the vector of outline points
+      // The vector holding the vectors of outline points
       // of the squares in the squares_ image
       std::vector< std::vector< cv::Point2f > > squaresOutlinePointsVector_;
 
@@ -216,7 +216,7 @@ namespace pandora_vision
       for ( int s = 0; s < squareOutlinePointsVector_.size(); s++ )
       {
         if (blobOutlineVector[b].x == squareOutlinePointsVector_[s].x
-        && blobOutlineVector[b].y == squareOutlinePointsVector_[s].y)
+          && blobOutlineVector[b].y == squareOutlinePointsVector_[s].y)
         {
           count_b_in_s++;
         }
