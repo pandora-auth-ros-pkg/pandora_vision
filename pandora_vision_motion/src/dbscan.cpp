bbf116aa93b3dbd9d129c34d9ff9221592b6345a
paschalidoud <paschalidoud@gmail.com>
2014-06-24 23:56:31 +0300

    Finished dbscan in motion detection!!To do add more generic implementation

--- pandora_vision_motion/src/dbscan.cpp
+++ pandora_vision_motion/src/dbscan.cpp
@@ -42,7 +42,7 @@ namespace pandora_vision
   /**
    @brief Class constructor 
   */
-  DBSCAN::DBSCAN(std::vector<cv::Rect>& data, double eps, int minPts)::_data(data)
+  DBSCAN::DBSCAN(std::vector<cv::Rect>& data, double eps, int minPts): _data(data)
   {
     _cluster_id = -1;
     _eps = eps;
@@ -63,8 +63,6 @@ namespace pandora_vision
   */ 
   void DBSCAN::init(unsigned int num_of_points)
   {
-    _labels.resize(num_of_points);
-    
     for(int i = 0; i < num_of_points; i++)
     {
       _labels[i] = -99;
@@ -107,57 +105,143 @@ namespace pandora_vision
   std::vector<int> DBSCAN::regionQuery(int p)
   {
     std::vector<int> res;
-    for(int i=0;i<data.size();i++)
+    for(int i = 0; i < _data.size(); i++)
     {
-        if(distanceFunc(p,i) <= _eps)
+        if(calculateDistanceMatrix(p,i) <= _eps)
             res.push_back(i);
     }
     return res;
   }
   
-  void DBSCAN::expandCluster(int p, vector<int> neighbours)
+  /**
+   @brief If a point is found to be a dense part of a cluster, its 
+   ε-neighborhood is also part of that cluster. Hence, all points that 
+   are found within the ε-neighborhood are added, as is their own ε-neighborhood when 
+   they are also dense. This process continues until the density-connected cluster 
+   is completely found.
+   @param p Point p, whose ε-neighborhood we are calculating
+   @param  neighbours, found neighbours for current point
+   @param void
+  */ 
+  void DBSCAN::expandCluster(int p, std::vector<int> neighbours)
+  {
+    _labels[p] = _cluster_id;
+    ///For each point in P' neighbor
+    for(int i = 0; i < neighbours.size(); i++)
     {
-      _labels[p] = _cluster_id;
-      for(int i=0;i<neighbours.size();i++)
+      ///If P' is not visited
+      if(!isVisited(neighbours[i]))
       {
-        if(!isVisited(neighbours[i]))
-        {
-            labels[neighbours[i]]=C;
-            std::vector<int> neighbours_p = regionQuery(neighbours[i]);
-            if (neighbours_p.size() >= _minPts)
-                expandCluster(neighbours[i],neighbours_p);
-        }
+        ///Mark P' as visited
+        _visitedPoints.at(neighbours[i]) = true;
+        
+        _labels[neighbours[i]] = _cluster_id;
+        std::vector<int> neighbours_p = regionQuery(neighbours[i]);
+        if (neighbours_p.size() >= _minPts)
+            expandCluster(neighbours[i], neighbours_p);
       }
+    }
+  }
+  /**
+   @brief Function that returns all clusters calculated from the given
+   set of points
+   @param void
+   @return vector of clusters 
+  */ 
+  std::vector<std::vector<cv::Rect> > DBSCAN::getGroups()
+  {
+    std::vector<std::vector<cv::Rect> > clusters;
+    for(int i = 0; i <= _cluster_id; i++)
+    {
+        clusters.push_back(std::vector<cv::Rect>());
+        for(int j = 0; j < _data.size(); j++)
+        {
+            if(_labels[j]==i)
+              clusters[clusters.size()-1].push_back(_data[j]);
+        }
+    }
+    return clusters;
+  }
+      
+  void DBSCAN::dbscan_cluster()
+  {
+    DP = new double[_data.size()* _data.size()];
+    for(int i = 0; i < _data.size(); i++)
+    {
+        for(int j = 0; j < _data.size(); j++)
+        {
+            if(i == j)
+                DP[i, j]=0;
+            else
+                DP[i, j]=-1;
+        }
+    }
+    for(int i = 0; i < _data.size(); i++){
+      if(isVisited(i) == false)
+      {
+        ///Mark current point as visited
+        _visitedPoints.at(i) = true;
+        std::vector<int> neighbours = regionQuery(i);
+          if(neighbours.size() < _minPts)
+          {
+            ///Mark P as noise
+            _labels[i] = -1;
+            _noise.push_back(i);
+          }
+          else
+          {
+            ///Expand cluster
+            _cluster_id++;
+            expandCluster(i, neighbours);
+          }
+      }
+    }  
   }
   
-  void DBSCAN::dbscan_cluster()
-      {
-          dp = new double[data.size()*data.size()];
-          for(int i=0;i<data.size();i++)
-          {
-              for(int j=0;j<data.size();j++)
-              {
-                  if(i==j)
-                      DP(i,j)=0;
-                  else
-                      DP(i,j)=-1;
-              }
-          }
-          for(int i=0;i<data.size();i++)
-          {
-              if(!isVisited(i))
-              {
-                  vector<int> neighbours = regionQuery(i);
-                  if(neighbours.size()<mnpts)
-                  {
-                      labels[i]=-1;//noise
-                  }else
-                  {
-                      C++;
-                      expandCluster(i,neighbours);
-                  }
-              }
-          }
-          delete [] dp;
-      }
+  double DBSCAN::calculateDistanceMatrix(int pt1, int pt2)
+  {
+    if(DP[pt1, pt2] != -1.0)
+        return DP[pt1, pt2];
+        
+    cv::Rect a = _data[pt1];
+    cv::Rect b = _data[pt2];
+   
+    cv::Point2d tla = cv::Point2d(a.x, a.y);
+    cv::Point2d tra = cv::Point2d(a.x + a.width, a.y);
+    cv::Point2d bla = cv::Point2d(a.x, a.y + a.height);
+    cv::Point2d bra = cv::Point2d(a.x + a.width, a.y + a.height);
+
+    cv::Point2d tlb = cv::Point2d(b.x, b.y);
+    cv::Point2d trb = cv::Point2d(b.x + b.width, b.y);
+    cv::Point2d blb = cv::Point2d(b.x, b.y + b.height);
+    cv::Point2d brb = cv::Point2d(b.x + b.width, b.y + b.height);
+
+    double minDist = 9999999;
+
+    minDist = std::min(minDist,dist2d(tla,tlb));
+    minDist = std::min(minDist,dist2d(tla,trb));
+    minDist = std::min(minDist,dist2d(tla,blb));
+    minDist = std::min(minDist,dist2d(tla,brb));
+
+    minDist = std::min(minDist,dist2d(tra,tlb));
+    minDist = std::min(minDist,dist2d(tra,trb));
+    minDist = std::min(minDist,dist2d(tra,blb));
+    minDist = std::min(minDist,dist2d(tra,brb));
+
+    minDist = std::min(minDist,dist2d(bla,tlb));
+    minDist = std::min(minDist,dist2d(bla,trb));
+    minDist = std::min(minDist,dist2d(bla,blb));
+    minDist = std::min(minDist,dist2d(bla,brb));
+
+    minDist = std::min(minDist,dist2d(bra,tlb));
+    minDist = std::min(minDist,dist2d(bra,trb));
+    minDist = std::min(minDist,dist2d(bra,blb));
+    minDist = std::min(minDist,dist2d(bra,brb));
+    
+    DP[pt1, pt2] = minDist;
+    DP[pt2, pt1] = minDist;
+    return DP[pt1, pt2];
+  }
+  
+  
 }// namespace pandora_vision
