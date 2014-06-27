7f1c1b21095fce877a3e7b32db8c8bae9f93a7f5
alexandros philotheou <alexandros.filotheou@gmail.com>
2014-06-27 05:52:23 +0300

    Alters hole_merger tests per changes in merge process
    	modified:   pandora_vision_hole_detector/include/hole_fusion_node/hole_fusion.h
    	modified:   pandora_vision_hole_detector/src/hole_fusion_node/hole_fusion.cpp
    	modified:   pandora_vision_hole_detector/test/unit/hole_fusion_node/hole_merger_test.cpp

--- pandora_vision_hole_detector/src/hole_fusion_node/hole_fusion.cpp
+++ pandora_vision_hole_detector/src/hole_fusion_node/hole_fusion.cpp
@@ -638,8 +638,26 @@ namespace pandora_vision
 
 
 
-  void HoleFusion::makeValidHolesUnique(
-    HolesConveyor* allHoles, std::map<int, float>* validHolesMap)
+  /**
+    @brief Takes as input a container of holes and a map of indices
+    of holes referring to the container to validity probabilities.
+    It outputs the most probable unique valid holes and a map
+    adjusted to fit the altered container of holes.
+
+    Inside the allHoles container, reside holes that have originated
+    from the Depth and RGB nodes, plus any merges between them. Having
+    acquired the validity probability of each one of them, this method
+    locates valid holes that refer to the same physical hole in space
+    inside the allHoles container and picks the one with the largest
+    validity probability.
+    @param[in,out] allHoles [HolesConveyor*] The conveyor of holes.
+    @param[in,out] validHolesMap [std::map<int, float>*] The std::map
+    that maps holes inside the allHoles conveyor to their validity
+    probability
+    @return void
+   **/
+  void HoleFusion::makeValidHolesUnique(HolesConveyor* allHoles,
+    std::map<int, float>* validHolesMap)
   {
     #ifdef DEBUG_TIME
     Timer::start("makeValidHolesUnique", "processCandidateHoles");
