a8578f0b33954fb08011913f80346a11e2778c3c
Aggelos Triadafillidis <aggelostriadafillidis@gmail.com>
2015-07-05 23:46:19 +0300

    apply edge detection in inputImage

--- pandora_vision_obstacle/src/hard_obstacle_detection/hard_obstacle_detector.cpp
+++ pandora_vision_obstacle/src/hard_obstacle_detection/hard_obstacle_detector.cpp
@@ -34,6 +34,7 @@
  *
  * Authors:
  *  Choutas Vassilis <vasilis4ch@gmail.com>
+ *  Angelos Triantafyllidis <aggelostriadafillidis@gmail.com>
  *********************************************************************/
 
 #include <string>
@@ -49,6 +50,96 @@ namespace pandora_vision
       const ros::NodeHandle& nh)
   {
     nodeName = name;
+  }
+
+  cv::Mat HardObstacleDetector::startDetection(const cv::Mat& inputImage)
+  {
+    if (show_input_image)
+    {
+      // Show the input image
+      showImage("The input image", inputImage, 1);
+    }
+
+    //detectEdges(inputImage, );
+
+  }
+
+  void HardObstacleDetector::showImage(
+    const std::string& title, const cv::Mat& image, int time)
+  {
+    // Copy the input image to another
+    cv::Mat imageCpy;
+    image.copyTo(imageCpy);
+
+    if (imageCpy.depth() == CV_8SC1)
+    {
+      image.convertTo(imageCpy, CV_8UC1);
+
+      // If value is negative (-1), make it 255 for visualization
+      for (unsigned int rows = 0; rows < image.rows; rows++)
+      {
+        for (unsigned int cols = 0; cols < image.cols; cols++)
+        {
+          if (image.at<signed char>(rows, cols) < 0)
+          {
+            imageCpy.at<unsigned char>(rows, cols) = 255;
+          }
+        }
+      }
+    }
+    cv::imshow(title, imageCpy);
+    cv::waitKey(time);
+  }
+
+  void HardObstacleDetector::applyCanny(
+    const cv::Mat& inImage, cv::Mat* outImage)
+  {
+
+  }
+
+  void HardObstacleDetector::applyScharr(
+    const cv::Mat& inImage, cv::Mat* outImage)
+  {
+
+  }
+
+  void HardObstacleDetector::applySobel(
+    const cv::Mat& inImage, cv::Mat* outImage)
+  {
+
+  }
+
+  void HardObstacleDetector::detectEdges(
+    const cv::Mat& inImage, cv::Mat* outImage)
+  {
+    // Copy the input image to another
+    cv::Mat imageCpy;
+    inImage.copyTo(imageCpy);
+
+    // Since the input image has CV_8S type, change it to CV_8UC1 in order to
+    // further process it.
+    // The input image had values in range [-1, 100] Occupancy grid map
+    // After conversion its range will be [0, 100]
+    imageCpy.convertTo(imageCpy, CV_8UC1);
+
+    switch (edge_method)
+    {
+      case 0 :
+        applyCanny(imageCpy, outImage);
+        break;
+      case 1 :
+        applyScharr(imageCpy, outImage);
+        break;
+      case 2 :
+        applySobel(imageCpy, outImage);
+        break;
+    }
+
+    if (show_edges_image)
+    {
+      showImage("The edges image", *outImage, 1);
+    }
+
   }
 
 }  // namespace pandora_vision
